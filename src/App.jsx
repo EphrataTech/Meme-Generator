dd3f4cc196915837b28f1b0f9035d5d56f1e99d3
Ephrata <ephrata0327@gmail.com>
2025-07-07 14:55:17 +0300

    feat: add header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 
 import './App.css'
+import Header from "./components/Header"
 
 export default function App() {
-  
-
-  return 
-  
+  return(
+    <div>
+      <Header />
+    </div>
+  )
 
 }
+  
+
+  
+  
+
+
